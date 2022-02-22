e4bcee128f2d5ad4d99181a861f12d364e97f872
Fabian Narvaez <fabiannarvaez@Fabians-MacBook-Pro.local>
2022-02-22 13:15:57 -0500

    updating branch

--- src/chat.js
+++ src/chat.js
@@ -5,46 +5,73 @@ import ListChats from "./components/listChats";
 import ChatContainer from "./components/chatContainer";
 import Box from "@mui/material/Box";
 import { useHistory } from "react-router-dom";
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3001");
 
 function Chat() {
-  const history = useHistory();
-  useEffect(() => {
-    Axios.get("http://localhost:3001/login").then((response) => {
-      if (!response.data.loggedIn) {
-        console.log("user is logged in");
+	const history = useHistory();
+	const [userFriends, setUserFriends] = useState([]);
+	const [messageTo, setMessageTo] = useState();
+	const [currentUserChat, setCurrentUserChat] = useState("");
 
-        return history.push("/");
-      }
-    });
-  }, []);
+	useEffect(() => {
+		isAuthenticated();
+	}, []);
 
-  Axios.defaults.withCredentials = true;
-  return (
-    <Layout>
-      {" "}
-      <Box
-        sx={{
-          display: "flex",
-        }}
-      >
-        <Box
-          sx={{
-            width: 300,
-            border: 1,
-          }}
-        >
-          <ListChats />
-        </Box>
-        <Box
-          sx={{
-            flexGrow: 1,
-          }}
-        >
-          <ChatContainer />
-        </Box>
-      </Box>
-    </Layout>
-  );
+	Axios.defaults.withCredentials = true;
+
+	const isAuthenticated = () => {
+		Axios.get("http://localhost:3001/login").then((response) => {
+			if (!response.data.loggedIn) {
+				return history.push("/");
+			}
+
+			socket.emit("joined", response.data.user._id);
+
+			Axios.get(
+				`http://localhost:3001/users/${response.data.user._id}/friendships`
+			).then((response) => {
+				setUserFriends(response.data.data.friendships);
+			});
+		});
+	};
+
+	const getCurrentChat = (event) => {
+		setMessageTo(event.currentTarget.id);
+		setCurrentUserChat(event.currentTarget.innerText);
+	};
+
+	return (
+		<Layout>
+			{" "}
+			<Box
+				sx={{
+					display: "flex",
+				}}
+			>
+				<Box
+					sx={{
+						width: 300,
+						border: 1,
+					}}
+				>
+					<ListChats existingFriends={userFriends} openChat={getCurrentChat} />
+				</Box>
+				<Box
+					sx={{
+						flexGrow: 1,
+					}}
+				>
+					<ChatContainer
+						socket={socket}
+						userName={currentUserChat}
+						messageTo={messageTo}
+					/>
+				</Box>
+			</Box>
+		</Layout>
+	);
 }
 
 export default Chat;
--- src/components/chatContainer.js
+++ src/components/chatContainer.js
@@ -1,30 +1,68 @@
+import React, { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import ChatContainerHeader from "./chatHeader";
 import { TextField } from "@mui/material";
 import { Button } from "@mui/material";
 
 const ChatContainer = ({ socket, userName, messageTo }) => {
-  return (
-    <Box sx={{}}>
-      <ChatContainerHeader userName={userName} userId={messageTo} />
-      <Box sx={{ height: 550 }}></Box>
-      <Box
-        sx={{
-          display: "flex",
-          m: 1,
-        }}
-      >
-        <TextField fullWidth label="Enter a message.." id="fullWidth" />
-        <Button
-          variant="contained"
-          color="success"
-          sx={{ pl: 3, pr: 3, ml: 1 }}
-        >
-          Send
-        </Button>
-      </Box>
-    </Box>
-  );
+	const [currentMessage, setCurrentMessage] = useState("");
+	const [messageList, setMessageList] = useState([]);
+
+	useEffect(() => {
+		socket.on("recieve-message", (data) => {
+			console.log(data);
+			setMessageList((list) => [...list, data]);
+		});
+	}, [socket]);
+
+	const sendMessage = async () => {
+		if (currentMessage !== "") {
+			const messageData = {
+				messageTo: messageTo,
+				message: currentMessage,
+			};
+
+			await socket.emit("send-message", messageData);
+			setMessageList((list) => [...list, messageData]);
+			setCurrentMessage("");
+		}
+	};
+	return (
+		<Box sx={{}}>
+			<ChatContainerHeader userName={userName} userId={messageTo} />
+			<Box sx={{ height: 550 }}>
+				<div>
+					{messageList?.map((message, index) => {
+						return <p key={index}>{message.message}</p>;
+					})}
+				</div>
+			</Box>
+			<Box
+				sx={{
+					display: "flex",
+					m: 1,
+				}}
+			>
+				<TextField
+					fullWidth
+					label="Enter a message.."
+					id="fullWidth"
+					value={currentMessage}
+					onChange={(event) => {
+						setCurrentMessage(event.target.value);
+					}}
+				/>
+				<Button
+					variant="contained"
+					color="success"
+					sx={{ pl: 3, pr: 3, ml: 1 }}
+					onClick={sendMessage}
+				>
+					Send
+				</Button>
+			</Box>
+		</Box>
+	);
 };
 
 export default ChatContainer;
