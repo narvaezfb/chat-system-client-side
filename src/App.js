9748c9fe023a16d8e5d87c2a916bb9b35500d639
Fabian Narvaez <fabiannarvaez@Fabians-MacBook-Pro.local>
2022-03-07 22:02:28 -0500

    merging changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Chat from "./chat";
 import Login from "./login";
+import Profile from "./userProfile";
 
 function App() {
 	return (
 		<Router>
 			<Switch>
+				<Route path="/login">
+					<Login />
+				</Route>
 				<Route path="/chat">
 					<Chat />
 				</Route>
-				<Route path="/">
-					<Login />
+				<Route path="/user/userProfile">
+					<Profile />
 				</Route>
 			</Switch>
 		</Router>
---
+++ src/userProfile.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const UserInfo = (props) => {
+	// const { img, name, username, email, phone, salary } = props.user;
+	return (
+		<div>
+			<div className="card">
+				{/* <img src={img} alt="" />
+                <h2>{name}</h2>
+                <h4>Username: {username}</h4>
+                <h4>Email: {email}</h4>
+                <h4>Phone: {phone}</h4>
+                <h4 className="salary">Salary: {salary}</h4>
+                <button onClick={() => props.handleAddBtn(props.user)}>Add</button> */}
+				<h1>settings page</h1>
+			</div>
+		</div>
+	);
+};
+
+export default UserInfo;
