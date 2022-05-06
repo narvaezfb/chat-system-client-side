aa18711cd98c0f766d42d5df049b6e51ce4fdbf0
Fabian Narvaez <fabiannarvaez@Fabians-MacBook-Pro.local>
2022-05-06 17:19:55 -0400

    editing login page

--- src/login.js
+++ src/login.js
@@ -1,17 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Axios from "axios";
 import Box from "@mui/material/Box";
-import { TextField, Typography } from "@mui/material";
+import { TextField, Typography, Paper, Divider } from "@mui/material";
 import { Button } from "@mui/material";
 import { useHistory, useLocation } from "react-router-dom";
 import Link from "@mui/material/Link";
+import BackgroundImage from "./media/green-brick-2.jpeg";
 
 const Login = () => {
+	//state variables
 	const [userEmail, setUserEmail] = useState("");
 	const [userPassword, setUserPassword] = useState("");
-	const [message, setMessage] = useState("");
+	const [errorMessage, setErrorMessage] = useState([]);
 	// eslint-disable-next-line no-unused-vars
 	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+	//ref variables
+	const emailRef = useRef();
+	const passwordRef = useRef();
 
 	//history
 	const location = useLocation();
@@ -40,75 +46,114 @@ const Login = () => {
 			});
 
 		console.log(location.state);
-	}, [message]); // eslint-disable-line react-hooks/exhaustive-deps
+
+		//focus email input when page loads
+		emailRef.current.focus();
+	}, []); // eslint-disable-line react-hooks/exhaustive-deps
 
 	const login = async () => {
-		if (userEmail === "" || userPassword === "") {
-			return setMessage("please provide email and password");
+		var errors = [];
+
+		if (userEmail === "") {
+			errors.push("please enter email");
+			emailRef.current.focus();
 		}
 
-		await Axios.post(`${url}/login`, {
-			email: userEmail,
-			password: userPassword,
-		}).then((response) => {
-			if (response.data.auth) {
-				setIsAuthenticated(true);
-				localStorage.setItem("token", "Bearer " + response.data.token);
-				return history.push("/chat");
-			} else {
-				setMessage("Invalid credentials");
-			}
-		});
+		if (userPassword === "") {
+			errors.push("please enter password");
+			passwordRef.current.focus();
+		}
+
+		if (errors.length === 0) {
+			await Axios.post(`${url}/login`, {
+				email: userEmail,
+				password: userPassword,
+			}).then((response) => {
+				if (response.data.auth) {
+					setIsAuthenticated(true);
+					localStorage.setItem("token", "Bearer " + response.data.token);
+					return history.push("/chat");
+				} else {
+					errors.push("Invalid credentials, try again");
+					emailRef.current.focus();
+					return setErrorMessage(errors);
+				}
+			});
+		} else {
+			setErrorMessage(errors);
+		}
+	};
+
+	//This function will be in charge of clearing the error messages displayed on the page
+	const clearErrors = () => {
+		setErrorMessage([]);
+	};
+
+	//This function will allow user to submit the form by hitting enter keyword
+	const handleEnterKeyPress = (e) => {
+		//it triggers by pressing the enter key
+		if (e.keyCode === 13) {
+			login();
+		}
 	};
 
 	return (
 		<Box
 			sx={{
-				width: "100%",
-				height: "100%",
 				display: "flex",
 				justifyContent: "center",
 				alignItems: "center",
+				backgroundImage: `url(${BackgroundImage})`,
+				backgroundPosition: "center",
+				backgroundSize: "cover",
+				height: "98vh",
 			}}
 		>
-			<Box
+			<Paper
+				elevation={3}
 				sx={{
 					display: "flex",
 					flexDirection: "column",
 					justifyContent: "center",
-					marginTop: 20,
-					width: "25%",
+					width: "350px",
 					padding: 6,
-					border: 1,
+					border: 2,
+					borderBlockColor: "#1b5e20",
+					margin: 2,
 				}}
 			>
-				<Typography variant="h4" align="center">
-					{" "}
-					Sign In{" "}
-				</Typography>
 				<TextField
+					inputRef={emailRef}
 					required
 					id="outlined-required"
 					label="email"
 					sx={{ marginTop: 1 }}
-					onChange={(event) => setUserEmail(event.target.value)}
+					onChange={(event) => {
+						setUserEmail(event.target.value);
+						clearErrors();
+					}}
 				/>
 				<TextField
+					inputRef={passwordRef}
 					required
 					id="outlined-required"
 					label="password"
 					type={"password"}
 					sx={{ marginTop: 1 }}
-					onChange={(event) => setUserPassword(event.target.value)}
+					onChange={(event) => {
+						setUserPassword(event.target.value);
+						clearErrors();
+					}}
 				/>
 				<Button
 					variant="contained"
-					color="success"
-					sx={{ marginTop: 1 }}
+					sx={{ marginTop: 1, mb: 3, backgroundColor: "#1b5e20" }}
 					onClick={login}
+					onKeyPress={handleEnterKeyPress}
 				>
-					Submit
+					Log In
 				</Button>
+				<Divider variant="middle" />
 				<Box
 					sx={{
 						display: "flex",
@@ -123,15 +168,32 @@ const Login = () => {
 				<Box
 					sx={{
 						display: "flex",
+						flexDirection: "column",
 						justifyContent: "center",
 						alignItems: "center",
 						marginTop: 2,
 					}}
 				>
-					<Typography color="#FF0000"> {message}</Typography>
+					{errorMessage.map((messages, index) => {
+						return (
+							<Typography color="#f44336" key={index}>
+								{" "}
+								{messages}
+							</Typography>
+						);
+					})}
+				</Box>
+				<Box
+					sx={{
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center",
+						marginTop: 2,
+					}}
+				>
 					<Typography> {location.state?.message}</Typography>
 				</Box>
-			</Box>
+			</Paper>
 		</Box>
 	);
 };
