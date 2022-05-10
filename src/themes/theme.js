f677e7f12ab877d758f54d84b78105fefbb23a7f
Fabian Narvaez <fabiannarvaez@Fabians-MacBook-Pro.local>
2022-05-10 11:20:50 -0400

    changhing layout on login page with new colors and new icon

---
+++ src/themes/theme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material/styles";
+
+export const Theme = createTheme({
+	palette: {
+		primary: {
+			light: "#e3f2fd",
+			main: "#001E3C",
+		},
+		secondary: {
+			main: "#0d47a1",
+		},
+		green: {
+			main: "#1DB954",
+		},
+	},
+});
